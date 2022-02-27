81ab0f4c181cb8ab5028b4817f4caa68b631b01f
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-02-27 13:38:43 -0800

    Create container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Container from './components/Container';
+
 const App: React.FC = () => (
-  <div className="text-3xl font-bold underline">Hello World</div>
+  <div className="h-screen flex justify-center items-center bg-slate-100">
+    <Container />
+  </div>
 );
 
 export default App;
---
+++ src/components/Container.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Container: React.FC = () => (
+  <section className="w-[300px] h-[400px] bg-gray-900 text-white font-RedHat" />
+);
+
+export default Container;
