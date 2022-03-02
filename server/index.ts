2f705676fdd4e532b6b0024ad006c7224f89765d
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-03-01 20:36:33 -0800

    Set up eslint

--- server/index.ts
+++ server/index.ts
@@ -5,5 +5,5 @@ const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
